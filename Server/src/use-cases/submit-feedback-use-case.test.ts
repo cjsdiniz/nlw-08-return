8042b4bcd952fa82aefe8d65887cc40a1072375b
cjsdiniz <cjsdiniz@outlook.com>
2022-05-09 23:50:47 +0100

    Testing

--- Server/src/use-cases/submit-feedback-use-case.test.ts
+++ Server/src/use-cases/submit-feedback-use-case.test.ts
@@ -5,10 +5,19 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
 describe("Submit feedback", () => {
-  it("should be able to submit a feedback", () => {
+  it("should be able to submit a feedback", async () => {
     const submitFeedback = new SubmitFeedbackUseCase(
       { create: async () => {} },
       { sendMail: async () => {} }
     );
+
+    expect(
+      await submitFeedback.execute({
+        type: "BUG",
+        comment: "example comment",
+        // screenshot: "test.jpg",
+        screenshot: "data:image/png;base64;akjhlkhkshk",
+      })
+    ).resolves.not.toThrow();
   });
 });
