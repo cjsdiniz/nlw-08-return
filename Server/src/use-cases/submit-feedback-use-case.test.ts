353f3c8e5d81ae7491ad471117ca3778e91c5458
cjsdiniz <cjsdiniz@outlook.com>
2022-05-10 15:37:09 +0100

    Tests + Cors

--- Server/src/use-cases/submit-feedback-use-case.test.ts
+++ Server/src/use-cases/submit-feedback-use-case.test.ts
@@ -4,20 +4,60 @@
 
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
-describe("Submit feedback", () => {
-  it("should be able to submit a feedback", async () => {
-    const submitFeedback = new SubmitFeedbackUseCase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    );
+// Spies
 
-    expect(
-      await submitFeedback.execute({
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  // { create: async () => {} },
+  // { sendMail: async () => {} }
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
+);
+
+describe("Submit feedback", () => {
+  it("should to be able to submit a feedback", async () => {
+    await expect(
+      submitFeedback.execute({
         type: "BUG",
         comment: "example comment",
         // screenshot: "test.jpg",
         screenshot: "data:image/png;base64;akjhlkhkshk",
       })
     ).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+  });
+  it("should not to be able to submit a feedback without type", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "",
+        comment: "example comment",
+        // screenshot: "test.jpg",
+        screenshot: "data:image/png;base64;akjhlkhkshk",
+      })
+    ).rejects.toThrow();
+  });
+  it("should not to be able to submit a feedback without comment", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "",
+        // screenshot: "test.jpg",
+        screenshot: "data:image/png;base64;akjhlkhkshk",
+      })
+    ).rejects.toThrow();
+  });
+  it("should not to be able to submit a feedback with invalid screenshot", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "example comment",
+        // screenshot: "test.jpg",
+        screenshot: "test.jpg",
+      })
+    ).rejects.toThrow();
   });
 });
